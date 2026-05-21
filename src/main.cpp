98a89996fbd825b9bdac5151d485cfc0ef189b4f
Timothy Leedberg <timothyleedberg@icloud.com>
2026-05-21 18:58:01 -0400

    First Graphics

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,28 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH,SCREEN_HEIGHT, &Wire, -1);
 
+int rot = 0;
+int level[8] = {0,0,0,0,0,1,0,0};
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(8,INPUT);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   delay(1000);
-  display.setTextColor(WHITE,BLACK);
   display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(5);
-  display.println("Hello");
-  display.display();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(250);
+  display.clearDisplay();
+  display.drawFastHLine(0,60,128,WHITE);
+  display.drawRotatedRect(24,52,16,16,rot,WHITE);
+  for(int i=0; i<8; i++) {
+    if(level[i] == 1){
+      display.drawTriangle(i*16,60,i*16+8,44,i*16+16,60,WHITE);
+    }
+  }
+  display.display();
 }
