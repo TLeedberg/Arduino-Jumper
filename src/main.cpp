4cc5080ffb4af584115a485417873ea05e905d75
Timothy Leedberg <timothyleedberg@icloud.com>
2026-05-21 19:17:22 -0400

    Added Jumping

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,17 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH,SCREEN_HEIGHT, &Wire, -1);
 
+int buttonPin = 8;
 int rot = 0;
 int level[8] = {0,0,0,0,0,1,0,0};
+bool jumping = false;
+bool directionUp = false;
+int height = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(8,INPUT);
+  Serial.begin(9600);
+  pinMode(buttonPin,INPUT);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   delay(1000);
@@ -22,14 +27,38 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(250);
+  delay(100);
+  if (digitalRead(buttonPin) == HIGH && !jumping) {
+    jumping=true;
+    directionUp = true;
+    height++;
+  }
+
+  if (jumping){
+    if(height>=20 && directionUp){
+      directionUp = false;
+    }
+
+    if (directionUp){
+      height = height+5;
+    } else{
+      height = height-5;
+    }
+  }
+  if(height<=0) {
+      jumping = false;
+      height=0;
+  }
+
   display.clearDisplay();
   display.drawFastHLine(0,60,128,WHITE);
-  display.drawRotatedRect(24,52,16,16,rot,WHITE);
+
+  display.drawRotatedRect(24,52-height,16,16,rot,WHITE);
   for(int i=0; i<8; i++) {
     if(level[i] == 1){
       display.drawTriangle(i*16,60,i*16+8,44,i*16+16,60,WHITE);
     }
   }
   display.display();
+  Serial.println(height);
 }
